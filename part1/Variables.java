140fdc80d8f6401f33d57d60913b9fe242e5d3ed
Somprakash04 <somprakasharun@gmail.com>
2025-03-15 22:31:37 +0530

    Variables

---
+++ part1/Variables.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Variables {
+    public static void main(String[] args) {
+        String text = "contains text";
+        //int number = 123;
+        double decimal = 12.34;
+        boolean booleanValue = true;
+        System.out.println("Text: "+ text);
+       // System.out.println("integer number: "+ number);
+        System.out.println("decimal number: "+ decimal);
+        System.out.println("boolean value: "+ booleanValue);
+// Changing a value assigned to a variable
+       int number = 123;
+       System.out.println("number: "+ number);
+       number = 456;
+       System.out.println("number: "+ number);
+
+// //Variables type Persists----------------
+//     // boolean value = true;    
+//     // value = 10;// does not work
+//     // int value = 10;
+//     // inte = value;//does not work
+//     //double value =12.34;
+//     //value = 10;// does  work
+//     //System.out.println("value: "+ value);
+//     // int 7variable = 4; // Not allowed!
+//     // int variable7 = 4; // Allowed, but is not a descriptive variable name
+//     // int System.out.print = 4; // Not Allowed!
+//     // int System.out.println = 4; // Not Allowed!
+
+// //Reading Different Variable Types from User------------
+//    Scanner scanner = new Scanner(System.in);
+//    System.out.print("Enter a text: ");
+//    String text = scanner.nextLine();
+//    System.out.println("your wrote: "+ text); 
+//    System.out.println("enter a value: ");
+//    int value = scanner.nextInt();
+//    System.out.println("your wrote value: "+ value);
+}
+}
+
+
