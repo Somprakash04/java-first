e9ce98cfbb95f3a303689feb4591e645af342270
Somprakash04 <somprakasharun@gmail.com>
2025-03-17 21:00:30 +0530

    Recurring problems and patterns

---
+++ part2/RecurringPatterns.java
@@ -0,0 +1,8 @@
+public class RecurringPatterns{
+    public static void main(String[] args){
+        
+    int number = 16;
+    double squareRoot = Math.sqrt(number);
+    System.out.println(squareRoot);
+    }
+}
