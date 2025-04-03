a0a5d1e041c0aad1a59446ff7f79fa8157e44f9b
Somprakash04 <somprakasharun@gmail.com>
2025-04-03 22:54:08 +0530

    Files and Reading

---
+++ part4/FilesandReading.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FilesandReading {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+while (true) {
+    String line = scanner.nextLine();
+
+    if (line.equals("end")) {
+        break;
+    }
+    System.out.println("line: " + line);
+
+    // add the read line to a list for later
+    // handling or handle the line immediately
+
+}
+
+    }
+}
